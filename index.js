5253d7ea2a996abe7f9a214e2c10a1db3afa091f
Daniel Mazzeu Dev <danielmazzeu.dev@gmail.com>
2026-02-11 09:06:50 -0300

    upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 4000; // Alterei para não conflitar com o Next.js
+const PORT = 4000;
 
 app.use(cors());
 
