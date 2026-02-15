cdab5d762a2d7370b0325559593fb7dfa5180913
Daniel Mazzeu Dev <danielmazzeu.dev@gmail.com>
2026-02-15 10:25:01 -0300

    Update

--- python/auto.bat
+++ python/auto.bat
@@ -2,17 +2,9 @@
 
 cd /d "C:\Users\danmz\Documents\GitHub\api_megasena\python"
 
-echo Iniciando o script Python em: %cd%
 python megasena.py
-
-echo.
-echo Script finalizado. Iniciando atualizacao no Git...
-
 git add .
 git commit -am "Update"
 git push
-
-echo.
-echo Processo concluido! Tudo pronto.
 
 exit
